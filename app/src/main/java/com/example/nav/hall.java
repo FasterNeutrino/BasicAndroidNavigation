e7f6e3e60fc1bf9683fc110ccf8a9a9ac37613a1
FasterNeutrino <48844443+FasterNeutrino@users.noreply.github.com>
2019-03-27 11:31:10 -0500

    Initial commit

---
+++ app/src/main/java/com/example/nav/hall.java
@@ -0,0 +1,38 @@
+package com.example.nav;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.navigation.Navigation;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class hall extends Fragment {
+
+
+    public hall() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hall, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.button2).setOnClickListener(Navigation.createNavigateOnClickListener(R.id.oneStep, null));
+    }
+
+}
